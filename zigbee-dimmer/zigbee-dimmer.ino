dee8b6d6d2ea495bb2034243758ef37c236be0e1
Oren Lederman <oren.lederman@gmail.com>
2017-01-08 10:00:11 -0500

    some cleanup

--- zigbee-dimmer/zigbee-dimmer.ino
+++ zigbee-dimmer/zigbee-dimmer.ino
@@ -9,13 +9,27 @@ unsigned char LED_PIN = 13;
 unsigned char dimming = 3;  // Dimming level (0-100)
 unsigned char i;
 
-volatile boolean int_called = false;
 volatile int pwm_value = 0;
 volatile int prev_time = 0;
+
 int on_off_value = 0;
+int temp_pwm_value;
+int temp_dimming;
+int prev_dimmering = dimming;
+
+// smoothing readings
+const int numReadings = 10;
+int readings[numReadings];      // the readings from the analog input
+int readIndex = 0;              // the index of the current reading
+int total = 0;                  // the running total
+int average = 0;                // the average
+
 
 void setup() {
-  pinMode(AC_LOAD_PIN, OUTPUT);// Set AC Load pin as output
+  //Serial.begin(9600);
+
+  // Set AC Load pin as output
+  pinMode(AC_LOAD_PIN, OUTPUT);
   
   // Dimmer sync. Pin 2 on Uno, Pin 2 on attiny
   attachInterrupt(0, zero_crosss_int, RISING);
@@ -29,44 +43,60 @@ void setup() {
   // initialize the digital pin as an output.
   pinMode(LED_PIN, OUTPUT);  
 
-  //Serial.begin(9600);
+  // Smoothing array
+  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+    readings[thisReading] = 0;
+  }
 }
 
 
-void loop() {
-  if (int_called) {
-    int_called = false;
-  }  
+void loop() {  
 
   on_off_value = digitalRead(ONOFF_IN_PIN);
   if (on_off_value == LOW) {
     dimming = 0;
   } else {
-    // almost full on = 50
-    // mid-way - 500
-    // low = 900
-    int temp_pwm_value = pwm_value;
-  
-    // Make sure we don't see any unexpected values
-    if (temp_pwm_value > 890) {
-      temp_pwm_value = 890;
-    } else if (temp_pwm_value < 0) {
-      temp_pwm_value = 0;
-    }
-  
-    int temp_dimming = map(temp_pwm_value, 0, 900, 10, 114);;
+      
+    // Get PWM and make sure we don't see any unexpected values
+    temp_pwm_value = pwm_value;
+    temp_pwm_value = constrain(temp_pwm_value, 0, 890); 
+    temp_pwm_value = add_reading(temp_pwm_value);
+
+    // Convert to values used by the dimmer circuit
+    temp_dimming = map(temp_pwm_value, 0, 890, 10, 114);;
   
     // Handle values that the specific LED bulb does not light
-    if (temp_dimming > 110) {
-      temp_dimming = 110;
-    }
+    temp_dimming = constrain(temp_dimming, 10, 114); 
     dimming = temp_dimming;
-  
     // Serial prints will mess up the timing
     //Serial.print(temp_pwm_value);
     //Serial.print(" - ");
     //Serial.println(temp_dimming);
   }
+}
+
+/*******************************************************/
+// Smooth
+// https://www.arduino.cc/en/tutorial/smoothing
+/*******************************************************/
+int add_reading(int reading) {
+  // subtract the last reading:
+  total = total - readings[readIndex];
+  // read from the sensor:
+  readings[readIndex] = reading;
+  // add the reading to the total:
+  total = total + readings[readIndex];
+  // advance to the next position in the array:
+  readIndex = readIndex + 1;
+
+  // if we're at the end of the array...
+  if (readIndex >= numReadings) {
+    readIndex = 0;
+  }
+
+  // calculate the average:
+  average = total / numReadings;
+  return average;
 }
 
 /*******************************************************/
@@ -77,8 +107,6 @@ void zero_crosss_int()  // function to be fired at the zero crossing to dim the 
   // Firing angle calculation : 1 full 50Hz wave =1/50=20ms 
   // Every zerocrossing : (50Hz)-> 10ms (1/2 Cycle) For 60Hz (1/2 Cycle) => 8.33ms 
   // 10ms=10000us
-
-  int_called = true;
   int dimtime = (65*dimming);    // For 60Hz =>65    
   delayMicroseconds(dimtime);    // Off cycle
   digitalWrite(AC_LOAD_PIN, HIGH);   // triac firing

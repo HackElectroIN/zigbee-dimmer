03cb3b3e0041e62e35c6360fc2754a775fdc8f16
Oren Lederman <oren.lederman@gmail.com>
2017-01-10 21:59:35 -0500

    copied updated code from the pin-change code

--- zigbee-dimmer/zigbee-dimmer.ino
+++ zigbee-dimmer/zigbee-dimmer.ino
@@ -1,3 +1,6 @@
+#include "RunningAverage.h"
+RunningAverage smoother(15);
+
 unsigned char AC_LOAD_PIN = 1;    // Output to Opto Triac pin
 unsigned char SYNC_PIN = 2; // Need pin 2 or 3 for interrupts
 
@@ -6,7 +9,6 @@ unsigned char ONOFF_IN_PIN = 4;
 
 volatile unsigned char dimming = 10;  // Dimming level (0-100)
 volatile boolean interrupted = false; // the dimmer interrupt sometimes messes up the PWM readings.
-unsigned char i;
 
 volatile int pwm_value = 0;
 volatile int prev_time = 0;
@@ -14,45 +16,52 @@ volatile int prev_time = 0;
 int on_off_value = 0;
 int temp_pwm_value;
 unsigned char temp_dimming;
-int prev_dimmering = dimming;
 
 void setup() {
-  //Serial.begin(9600);
+  Serial.begin(9600);
 
   // Set AC Load pin as output
   pinMode(AC_LOAD_PIN, OUTPUT);
   
   // Dimmer sync. Pin 2 on Uno, Pin 2 on attiny
-  pinMode(SYNC_PIN, INPUT);  
+  //pinMode(SYNC_PIN, INPUT);  
   attachInterrupt(digitalPinToInterrupt(SYNC_PIN), zero_crosss_int, RISING);
 
   // PIN for reading PWM from CREE bulb circuit
-  pinMode(PWM_IN_PIN, INPUT);  
+  //pinMode(PWM_IN_PIN, INPUT);  
   attachInterrupt(digitalPinToInterrupt(PWM_IN_PIN), falling, FALLING);
 
   pinMode(ONOFF_IN_PIN, INPUT);   
 }
 
-
+// dimming = 10 is brightest
+// dimming = 114 is darkest
 void loop() {  
   on_off_value = digitalRead(ONOFF_IN_PIN);
   if (on_off_value == LOW) {
     dimming = 0;
   } else {
-      
+    temp_pwm_value = pwm_value;
+    //Serial.println(temp_pwm_value );
+    
     // Get PWM and make sure we don't see any unexpected values
     temp_pwm_value = pwm_value;
-    temp_pwm_value = constrain(temp_pwm_value, 0, 890); 
+    if ((temp_pwm_value < 0) || (temp_pwm_value > 900)) {
+      return; 
+    }
+
+    // 
+    temp_pwm_value = constrain(temp_pwm_value, 10, 800); 
 
     // Convert to values used by the dimmer circuit
-    temp_dimming = map(temp_pwm_value, 0, 890, 10, 114);;
+    temp_dimming = map(temp_pwm_value, 10, 800, 10, 114);;
     temp_dimming = constrain(temp_dimming, 10, 114); 
+    smoother.addValue(temp_dimming );
+    temp_dimming  = smoother.getAverage();
 
-    dimming = temp_dimming;
-    // Serial prints will mess up the timing
-    //Serial.print(temp_pwm_value);
-    //Serial.print(" - ");
-    //Serial.println(temp_dimming);
+    dimming = temp_dimming;//temp_dimming;    
+    Serial.println(temp_dimming);
+
   }
 }
 
@@ -79,7 +88,6 @@ void zero_crosss_int()  // function to be fired at the zero crossing to dim the 
 void rising() {
   attachInterrupt(digitalPinToInterrupt(PWM_IN_PIN), falling, FALLING);
   prev_time = micros();
-  interrupted = false;
 }
  
 void falling() {
@@ -87,5 +95,6 @@ void falling() {
   if (! interrupted) {
     pwm_value = micros()-prev_time;
   }
+  interrupted = false;
 }
 

a515341d58b3beccd64fcf961c8349d4b8f29ff8
Oren Lederman <oren.lederman@gmail.com>
2017-01-15 19:25:37 -0500

    trying a new method. but still flickering on low (but doesn't BRIGHT up)

--- zigbee-dimmer/zigbee-dimmer.ino
+++ zigbee-dimmer/zigbee-dimmer.ino
@@ -1,21 +1,31 @@
 #include <AStar32U4.h>
+#include  <TimerOne.h> 
 
 #include "RunningAverage.h"
 RunningAverage smoother(15);
 
+// pins
 unsigned char AC_LOAD_PIN = 6;    // Output to Opto Triac pin
 unsigned char SYNC_PIN = 2; // Need pin 2 or 3 for interrupts
 
 unsigned char PWM_IN_PIN = 3; // Need pin 2 or 3 for interrupts
 unsigned char ONOFF_IN_PIN = 8;
 
+// Dimmer stuff
 unsigned char SHUTDOWN_VALUE = 60; // when to turn off the bulb (because of flickering..)
+volatile boolean zero_cross=0;  // Boolean to store a "switch" to tell us if we have crossed zero
+int freqStep = 65;    // This is the delay-per-brightness step in microseconds.
+                      // For 60 Hz it should be 65
 
-volatile unsigned char dimming = 114;  // Dimming level (0-100)
+volatile int i=0;               // Variable to use as a counter
+unsigned char dimming = 114;  // Dimming level (0-100)
+
+//PWM reading stuff
 volatile boolean interrupted = false; // the dimmer interrupt sometimes messes up the PWM readings.
 volatile int pwm_value = 0;
 volatile unsigned long prev_time = 0;
 
+// variables used in the loop
 int on_off_value = 0;
 int temp_pwm_value;
 int prev_temp_pwm_value = -1;
@@ -23,20 +33,28 @@ int prev_dimming = -1;
 unsigned char temp_dimming;
 
 void setup() {
-  //Serial.begin(9600);
+  Serial.begin(9600);
   
   // Set AC Load pin as output
   pinMode(AC_LOAD_PIN, OUTPUT);
   
   // Dimmer sync. Pin 2 on Uno, Pin 2 on attiny
   //pinMode(SYNC_PIN, INPUT);  
-  attachInterrupt(digitalPinToInterrupt(SYNC_PIN), zero_crosss_int, RISING);
+  attachInterrupt(digitalPinToInterrupt(SYNC_PIN), zero_cross_detect, RISING);
 
   // PIN for reading PWM from CREE bulb circuit
   //pinMode(PWM_IN_PIN, INPUT);  
   attachInterrupt(digitalPinToInterrupt(PWM_IN_PIN), falling, FALLING);
 
-  pinMode(ONOFF_IN_PIN, INPUT);   
+  pinMode(ONOFF_IN_PIN, INPUT); 
+
+  // Use the TimerOne Library to attach an interrupt
+  // to the function we use to check to see if it is 
+  // the right time to fire the triac.  This function 
+  // will now run every freqStep in microseconds.     
+  Timer1.initialize(freqStep);                      // Initialize TimerOne library for the freq we need
+  Timer1.attachInterrupt(dim_check, freqStep);      
+
 }
 
 // dimming = 10 is brightest
@@ -45,7 +63,7 @@ void loop() {
   on_off_value = digitalRead(ONOFF_IN_PIN);
   //Serial.println(on_off_value );
   if (on_off_value == LOW) {
-    dimming = 0;
+    dimming = 128;
   } else {
     temp_pwm_value = pwm_value;
     if (prev_temp_pwm_value != temp_pwm_value) {
@@ -61,7 +79,6 @@ void loop() {
   
       // Convert to values used by the dimmer circuit
       temp_dimming = map(temp_pwm_value, 10, 800, 10, 114);;
-      temp_dimming = constrain(temp_dimming, 10, 114); 
       smoother.addValue(temp_dimming );
       temp_dimming  = smoother.getAverage();
 
@@ -75,37 +92,25 @@ void loop() {
 /*******************************************************/
 // Dimmer
 /*******************************************************/
-void zero_crosss_int()  // function to be fired at the zero crossing to dim the light
-{
-  noInterrupts();
-  interrupted = true;
-  // Firing angle calculation : 1 full 50Hz wave =1/50=20ms 
-  // Every zerocrossing : (50Hz)-> 10ms (1/2 Cycle) For 60Hz (1/2 Cycle) => 8.33ms 
-  // 10ms=10000us
-  int dimtime = (65*dimming);    // For 60Hz =>65    
-  //unsigned long start = micros();
-  delayMicroseconds(dimtime);    // Off cycle
-  //unsigned long mid = micros();
-  digitalWrite(AC_LOAD_PIN, HIGH);   // triac firing
-  delayMicroseconds(8.33);         // triac On propogation delay (for 60Hz use 8.33)
-  //unsigned long endtime = micros();
-  digitalWrite(AC_LOAD_PIN, LOW);    // triac Off
-  //long b1 = mid - start;
-  //long b2 = endtime - mid;
-  /*
-  if (b1 < 0 or b2 <0) {
-    Serial.print(b1);
-    Serial.print(",");
-    Serial.print(b2);
-    Serial.print(",");
-    Serial.print(dimming);
-    Serial.print(",");
-    Serial.println(dimtime);
-  }
-  */
-  interrupts();
-}
+void zero_cross_detect() {    
+  zero_cross = true;               // set the boolean to true to tell our dimming function that a zero cross has occured
+  i=0;
+  digitalWrite(AC_LOAD_PIN, LOW);       // turn off TRIAC (and AC)
+}   
 
+// Turn on the TRIAC at the appropriate time
+void dim_check() {                   
+  if(zero_cross == true) {              
+    if(i>=dimming) {                     
+      digitalWrite(AC_LOAD_PIN, HIGH); // turn on light       
+      i=0;  // reset time step counter                         
+      zero_cross = false; //reset zero cross detection
+    } 
+    else {
+      i++; // increment time step counter                     
+    }                                
+  }                                  
+}                 
 /*******************************************************/
 // PWM-in functions
 /*******************************************************/
